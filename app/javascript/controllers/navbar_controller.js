e1d806a6f2ae2915707472f5cc0145da84e95dda
TheZero0-ctrl <ankitpariyar486@gmail.com>
2023-10-08 18:37:07 +0545

    make homepage nav responsive

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = [ "menu", "ham", "content" ]
+  static targets = [ "menu", "ham", "content", "hamContainer" ]
   connect() {
   }
 
@@ -10,13 +10,19 @@ export default class extends Controller {
     this.menuTarget.classList.toggle("hidden")
     this.hamTarget.classList.toggle("hidden")
     this.contentTarget.classList.toggle("hidden")
+    this.toggleHamContainer()
   }
 
   close() {
     if (window.innerWidth > 1024 && !this.menuTarget.classList.contains("hidden")) {
-      console.log(this.menuTarget)
       this.menuTarget.classList.toggle("hidden")
       this.contentTarget.classList.toggle("hidden")
+      this.toggleHamContainer()
+    }
+  }
+  toggleHamContainer() {
+    if (this.hasHamContainerTarget) {
+      this.hamContainerTarget.classList.toggle("hidden")
     }
   }
 }
