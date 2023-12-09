da9e7f706ca355aca5d59e095995835854bf15e6
TheZero0-ctrl <ankitpariyar486@gmail.com>
2023-12-10 00:03:37 +0545

    revise deploy file and add nginx.conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,20 @@
-# config valid for current version and patch releases of Capistrano
 lock '~> 3.18.0'
-
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
 
 set :rbenv_type, :user
 set :rbenv_ruby, '3.2.1'
 
-set :application, 'skillfull_square'
+set :application, 'skillful_square'
 set :repo_url, 'git@github.com:TheZero0-ctrl/skillful_square.git'
-set :deploy_to, '/home/deploy/apps'
+set :deploy_to, '/home/deploy/apps/skillful_square'
 set :branch, 'main'
 
-# set :linked_files, %w[config/secrets.yml]
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
 append :linked_files, 'config/master.key'
+# set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle .bundle public/system public/uploads node_modules]
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :initial, true
+before 'deploy:migrate', 'database:create' if fetch(:initial)
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+set :use_sudo, false
